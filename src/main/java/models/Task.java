1f2ceb180c6f8016c46c0030129424611c3f770c
Terán Wadid <wadidjt@gmail.com>
2023-06-04 16:18:24 -0500

    Add model Task

---
+++ src/main/java/models/Task.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class Task {
+  private Long taskId;
+  private User user;
+  private String name;
+
+  @Getter(AccessLevel.NONE)
+  private Boolean completed;
+
+  private LocalDate completedDate;
+  private LocalDate dueDate;
+  private LocalTime specifiedTime;
+  private String description;
+  private Relevance relevance;
+  private Category category;
+  private RepeatTaskConfig repeatingConfig;
+
+  public Boolean isCompleted() {
+    return completed;
+  }
+}
