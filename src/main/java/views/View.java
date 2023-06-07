64629c5cda998bac379c06d0151dd7527c78c18d
Neoly Moreno <neolyamr@gmail.com>
2023-06-07 09:08:45 -0500

    Create view method: displayOneTask

--- src/main/java/views/View.java
+++ src/main/java/views/View.java
@@ -4,7 +4,10 @@ import models.Category;
 import models.Relevance;
 import models.Task;
 
+import java.util.Formatter;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class View {
   private static final String FORMAT = "%25s %18s %20s %28s %30s %n";
@@ -19,6 +22,35 @@ public class View {
 
   public static void display(String string) {
     System.out.println(string);
+  }
+
+  public static void displayOneTask(Task task) {
+      Formatter ftmHead = new Formatter();
+      Formatter ftmBody = new Formatter();
+
+    String formatHead = "%25s %20s %20s %20s %30s %20s %30s %40s %n";
+    if (task.getRepeatingConfig() != null) {
+
+      ftmHead.format("%15s %15s %15s %20s %n", "| TYPE", "INTERVAL", "LOCALDATE", "REPEAT CONFIG |");
+      ftmBody.format("%188s %12s %18s %16s",
+              task.getRepeatingConfig().getRepeatType(),
+              task.getRepeatingConfig().getRepeatInterval(),
+              task.getRepeatingConfig().getRepeatEndsAt(),
+              task.getRepeatingConfig().getRepeatOn());
+    }
+
+    putTitle("TASK:", task.getName().toUpperCase());
+    System.out.printf(formatHead, NAME, "COMPLETED DATE", DUEDATE, SPECTIME, DESCRIP, RELEVANCE, CATEGORY, "REPETITIVE CONFIG");
+    lines();
+    System.out.printf(formatHead,
+            cutString(task.getName()),
+            task.getCompletedDate(),
+            task.getDueDate(),
+            task.getSpecifiedTime(),
+            cutString(task.getDescription()),
+            task.getRelevance(),
+            cutString(task.getCategory().getName()),
+            (task.getRepeatingConfig() == null ? "N/A" : ftmHead + "\n" + ftmBody));
   }
 
   public static void displayPendingTasksForToday(List<Task> tasks) {
@@ -39,20 +71,19 @@ public class View {
   public static void displayAllPendingTasks(List<Task> tasks) {
     putTitle("PENDING TASKS");
     final String HEAD = "%15s %28s %20s %18s %30s %12s %30s %n";
-    lines();
     System.out.printf(HEAD, "TASK ID", NAME, DUEDATE, SPECTIME, DESCRIP, RELEVANCE, CATEGORY);
     lines();
     // iterates over the list
     for (Task task : tasks) {
       System.out.format(
-          HEAD,
-          task.getTaskId(),
-          cutString(task.getName()),
-          task.getDueDate(),
-          task.getSpecifiedTime(),
-          cutString(task.getDescription()),
-          task.getRelevance(),
-          cutString(task.getCategory().getName()));
+              HEAD,
+              task.getTaskId(),
+              cutString(task.getName()),
+              task.getDueDate(),
+              task.getSpecifiedTime(),
+              cutString(task.getDescription()),
+              task.getRelevance(),
+              cutString(task.getCategory().getName()));
     }
 
     lines();
@@ -62,98 +93,99 @@ public class View {
     String format = "%25s %20s %20s %30s %15s %30s%n";
     putTitle("COMPLETED TASKS");
     System.out.printf(
-        format, NAME, "COMPLETED DATE", SPECTIME, DESCRIP, RELEVANCE, CATEGORY);
+            format, NAME, "COMPLETED DATE", SPECTIME, DESCRIP, RELEVANCE, CATEGORY);
     lines();
     // iterates over the list
     for (Task task : tasks) {
       System.out.format(
-          format,
-          cutString(task.getName()),
-          task.getCompletedDate(),
-          task.getSpecifiedTime(),
-          cutString(task.getDescription()),
-          task.getRelevance(),
-          cutString(task.getCategory().getName()));
+              format,
+              cutString(task.getName()),
+              task.getCompletedDate(),
+              task.getSpecifiedTime(),
+              cutString(task.getDescription()),
+              task.getRelevance(),
+              cutString(task.getCategory().getName()));
     }
 
     lines();
   }
 
   public static void displayTasksByCategory(List<Task> tasks, Category category) {
-    putTitle("TASKS BY CATEGORY",category.getName().toUpperCase());
+    putTitle("TASKS BY CATEGORY", category.getName().toUpperCase());
     System.out.printf(FORMAT, NAME, DUEDATE, SPECTIME, DESCRIP, RELEVANCE);
     lines();
     for (Task task : tasks) {
       System.out.printf(
-          FORMAT,
-          cutString(task.getName()),
-          task.getDueDate(),
-          task.getSpecifiedTime(),
-          cutString(task.getDescription()),
-          task.getRelevance());
+              FORMAT,
+              cutString(task.getName()),
+              task.getDueDate(),
+              task.getSpecifiedTime(),
+              cutString(task.getDescription()),
+              task.getRelevance());
     }
     lines();
   }
 
   public static void displayTasksByRelevance(List<Task> tasks, Relevance relevance) {
-    putTitle("TASKS BY RELEVANCE",relevance.name());
+    putTitle("TASKS BY RELEVANCE", relevance.name());
     System.out.printf(FORMAT, NAME, DUEDATE, SPECTIME, DESCRIP, CATEGORY);
     lines();
 
     for (Task task : tasks) {
       System.out.printf(
-          FORMAT,
-          cutString(task.getName()),
-          task.getDueDate(),
-          task.getSpecifiedTime(),
-          cutString(task.getDescription()),
-          cutString(task.getCategory().getName()));
+              FORMAT,
+              cutString(task.getName()),
+              task.getDueDate(),
+              task.getSpecifiedTime(),
+              cutString(task.getDescription()),
+              cutString(task.getCategory().getName()));
     }
     lines();
   }
 
   public static void displayCategories(List<Category> categories) {
-    final String SEPARATOR = "-----------------------------------------------";
-    System.out.println(SEPARATOR);
+    String separator = "-----------------------------------------------";
+    System.out.println(separator);
     System.out.printf("%30s", "CATEGORIES \n");
-    System.out.println(SEPARATOR);
+    System.out.println(separator);
     System.out.printf("%25s %n", NAME);
-    System.out.println(SEPARATOR);
+    System.out.println(separator);
     for (Category category : categories) {
       System.out.printf("%25s %n", cutString(category.getName()));
     }
-    System.out.println(SEPARATOR);
+    System.out.println(separator);
   }
 
   private static String cutString(String text) {
     return (text.length() > 24 ? text.substring(0, 22) + "..." : text);
   }
 
-  private static void putTitle(String title,String plus){
+  private static void putTitle(String title, String plus) {
     lines();
-    System.out.printf("%95s", title + " " + plus +"\n");
+    System.out.printf("%95s", title + " " + plus + "\n");
     lines();
   }
-  private static void putTitle(String title){
+
+  private static void putTitle(String title) {
     lines();
-    System.out.printf("%95s", title +"\n");
+    System.out.printf("%95s", title + "\n");
     lines();
   }
 
   private static void displayTasks(List<Task> tasks) {
-    final String HEAD = "%25s %20s %18s %30s %12s %30s %n";
-    System.out.printf(HEAD, NAME, DUEDATE, SPECTIME, DESCRIP, RELEVANCE, CATEGORY);
+    String head = "%25s %20s %18s %30s %12s %30s %n";
+    System.out.printf(head, NAME, DUEDATE, SPECTIME, DESCRIP, RELEVANCE, CATEGORY);
     lines();
     // iterates over the list
     for (Task task : tasks) {
       System.out.format(
-          HEAD,
-          cutString(task.getName()),
-          task.getDueDate(),
-          task.getSpecifiedTime(),
-          cutString(task.getDescription()),
-          task.getRelevance(),
-          cutString(task.getCategory().getName()));
+              head,
+              cutString(task.getName()),
+              task.getDueDate(),
+              task.getSpecifiedTime(),
+              cutString(task.getDescription()),
+              task.getRelevance(),
+              cutString(task.getCategory().getName()));
     }
 
     lines();
@@ -161,6 +193,6 @@ public class View {
 
   private static void lines() {
     System.out.println(
-        "-------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
   }
 }
