3ddb77f36df7948f920d75b7e7851a2bc4d45750
Terán Wadid <wadidjt@gmail.com>
2023-06-08 12:29:57 -0500

    Add empty list checking to renameCategories and deleteCategories in CategoryController

--- src/main/java/controllers/CategoryController.java
+++ src/main/java/controllers/CategoryController.java
@@ -34,70 +34,79 @@ public class CategoryController {
   }
 
   public void renameCategory() {
-    Object[] categoriesArray =
-        crudService.findAllCategories().stream().map(Category::getName).toArray();
-
-    String category =
-        (String)
-            JOptionPane.showInputDialog(
-                null,
-                "Choose the category to rename",
-                "Rename category",
-                JOptionPane.INFORMATION_MESSAGE,
-                null,
-                categoriesArray,
-                categoriesArray[0]);
-
-    if (category == null) {
-      JOptionPane.showMessageDialog(null, "You have to choose a category!");
-    } else if (!crudService.checkCategoryName(category)) {
-      JOptionPane.showMessageDialog(null, "The category " + category + " doesn't exist.");
+    ArrayList<Category> categories = crudService.findAllCategories();
+    if (categories.isEmpty()) {
+      JOptionPane.showMessageDialog(null, "You don't have any categories created.");
     } else {
-      String newCategory = JOptionPane.showInputDialog("Insert a new name for the category: ");
+      Object[] categoriesArray = categories.stream().map(Category::getName).toArray();
 
-      if (newCategory.length() > 50) {
-        JOptionPane.showMessageDialog(null, "Category names cannot be longer than 50 characters!");
-      } else if (crudService.checkCategoryName(newCategory)) {
-        JOptionPane.showMessageDialog(null, "This category name is already in use!");
-      } else if (JOptionPane.showConfirmDialog(
-              null,
-              "Are you sure you want to rename \""
-                  + category
-                  + "\" category to \""
-                  + newCategory
-                  + "\"?")
-          == 0) {
-        Category oldCategory = crudService.getCategoryByName(category);
-        oldCategory.setName(newCategory);
-        crudService.saveCategory(oldCategory);
+      String category =
+          (String)
+              JOptionPane.showInputDialog(
+                  null,
+                  "Choose the category to rename",
+                  "Rename category",
+                  JOptionPane.INFORMATION_MESSAGE,
+                  null,
+                  categoriesArray,
+                  categoriesArray[0]);
+
+      if (category == null) {
+        JOptionPane.showMessageDialog(null, "You have to choose a category!");
+      } else if (!crudService.checkCategoryName(category)) {
+        JOptionPane.showMessageDialog(null, "The category " + category + " doesn't exist.");
+      } else {
+        String newCategory = JOptionPane.showInputDialog("Insert a new name for the category: ");
+
+        if (newCategory.length() > 50) {
+          JOptionPane.showMessageDialog(
+              null, "Category names cannot be longer than 50 characters!");
+        } else if (crudService.checkCategoryName(newCategory)) {
+          JOptionPane.showMessageDialog(null, "This category name is already in use!");
+        } else if (JOptionPane.showConfirmDialog(
+                null,
+                "Are you sure you want to rename \""
+                    + category
+                    + "\" category to \""
+                    + newCategory
+                    + "\"?")
+            == 0) {
+          Category oldCategory = crudService.getCategoryByName(category);
+          oldCategory.setName(newCategory);
+          crudService.saveCategory(oldCategory);
+        }
       }
     }
   }
 
   public void deleteCategory() {
-    Object[] categoriesArray =
-        crudService.findAllCategories().stream().map(Category::getName).toArray();
+    ArrayList<Category> categories = crudService.findAllCategories();
+    if (categories.isEmpty()) {
+      JOptionPane.showMessageDialog(null, "You don't have any categories created.");
+    } else {
+      Object[] categoriesArray = categories.stream().map(Category::getName).toArray();
 
-    String category =
-        (String)
-            JOptionPane.showInputDialog(
-                null,
-                "Choose the category to delete",
-                "Delete category",
-                JOptionPane.INFORMATION_MESSAGE,
-                null,
-                categoriesArray,
-                categoriesArray[0]);
+      String category =
+          (String)
+              JOptionPane.showInputDialog(
+                  null,
+                  "Choose the category to delete",
+                  "Delete category",
+                  JOptionPane.INFORMATION_MESSAGE,
+                  null,
+                  categoriesArray,
+                  categoriesArray[0]);
 
-    if (category == null) {
-      JOptionPane.showMessageDialog(null, "You have to choose a category!");
-    } else if (!crudService.checkCategoryName(category)) {
-      JOptionPane.showMessageDialog(null, "The category " + category + " doesn't exist.");
-    } else if (JOptionPane.showConfirmDialog(
-            null, "Are you sure you want to delete \"" + category + "\" category?")
-        == 0) {
-      Category categoryToDelete = crudService.getCategoryByName(category);
-      crudService.deleteCategory(categoryToDelete);
+      if (category == null) {
+        JOptionPane.showMessageDialog(null, "You have to choose a category!");
+      } else if (!crudService.checkCategoryName(category)) {
+        JOptionPane.showMessageDialog(null, "The category " + category + " doesn't exist.");
+      } else if (JOptionPane.showConfirmDialog(
+              null, "Are you sure you want to delete \"" + category + "\" category?")
+          == 0) {
+        Category categoryToDelete = crudService.getCategoryByName(category);
+        crudService.deleteCategory(categoryToDelete);
+      }
     }
   }
 
