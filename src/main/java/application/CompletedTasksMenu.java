a4aca25d01a99f8ff166089f9c9487c84d349351
WarrenSanchez <diemendozac@gmail.com>
2023-06-09 13:11:50 -0500

    Add CompletedTasksMenu and add methods to SingletonMenuFactory
    - Add getSearchTasksMenu
    - Add getCompletedTasksMenu

---
+++ src/main/java/application/CompletedTasksMenu.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.HashMap;
+import services.*;
+
+public class CompletedTasksMenu extends AbstractMenu {
+  protected CompletedTasksMenu(
+      CRUDServiceImpl crudService,
+      FilteredTaskSearchService searchService,
+      UserService userService,
+      TaskService taskService,
+      CategoryService categoryService) {
+    super(crudService, searchService, userService, taskService, categoryService);
+    title = "Completed Tasks Menu";
+  }
+
+  @Override
+  public void configureMenuOptions() {
+    menuOptions = new HashMap<>();
+    menuOptions.put("View All Completed Tasks", 1);
+    menuOptions.put("Set A Completed Task As Pending", 2);
+    menuOptions.put("Delete All Completed Task", 3);
+    menuOptions.put("Back", 4);
+  }
+
+  @Override
+  public AbstractMenu options(int optionIndex) {
+    switch (optionIndex) {
+      case 1 -> {
+        taskService.searchCompletedTasks();
+        return this;
+      }
+      case 2 -> {
+        taskService.setAsPendingTask();
+        return this;
+      }
+      case 3 -> {
+        taskService.deleteCompletedTasks();
+        return this;
+      }
+      case 4 -> {
+        return SingletonMenuFactory.getTaskMenu(
+                crudService, searchService, userService, taskService, categoryService);
+      }
+      default -> {
+        return null;
+      }
+    }
+  }
+}
--- src/main/java/application/SingletonMenuFactory.java
+++ src/main/java/application/SingletonMenuFactory.java
@@ -8,6 +8,8 @@ public class SingletonMenuFactory {
   private static AbstractMenu taskMenu;
   private static AbstractMenu categoryMenu;
   private static AbstractMenu accountSettingsMenu;
+  private static AbstractMenu completedTaskMenu;
+  private static AbstractMenu searchTasksMenu;
 
   private SingletonMenuFactory() {}
 
@@ -75,5 +77,33 @@ public class SingletonMenuFactory {
               crudService, searchService, userService, taskService, categoryService);
     }
     return accountSettingsMenu;
+  }
+
+  public static AbstractMenu getSearchTasksMenu(
+      CRUDServiceImpl crudService,
+      FilteredTaskSearchService searchService,
+      UserService userService,
+      TaskService taskService,
+      CategoryService categoryService) {
+    if (searchTasksMenu == null) {
+      searchTasksMenu =
+          new SearchTasksMenu(
+              crudService, searchService, userService, taskService, categoryService);
+    }
+    return searchTasksMenu;
+  }
+
+  public static AbstractMenu getCompletedTasksMenu(
+      CRUDServiceImpl crudService,
+      FilteredTaskSearchService searchService,
+      UserService userService,
+      TaskService taskService,
+      CategoryService categoryService) {
+    if (completedTaskMenu == null) {
+      completedTaskMenu =
+          new CompletedTasksMenu(
+              crudService, searchService, userService, taskService, categoryService);
+    }
+    return completedTaskMenu;
   }
 }
